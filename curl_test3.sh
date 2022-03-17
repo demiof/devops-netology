764c0403ab7f6a2acfa95cae91a86200838dfb60
Denis <demi@corp.linkintel.ru>
2022-03-17 10:42:29 +0300

    BASH 4.1

---
+++ curl_test3.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+log="curl3.log"
+log_err="curl3_err.log"
+declare -i n=5
+declare -a a=("173.194.222.113" "87.250.250.242")
+
+if [ -f $log ] 
+	then
+	/usr/bin/rm $log
+fi
+if [ -f $log_err ]
+	then
+	/usr/bin/rm $log_err
+fi
+
+touch $log
+touch $log_err
+
+for i in ${a[@]} 
+do
+	declare -i j=1
+	while  ((j<=$n)) 
+	do
+	
+		nc -w1 -z $i 80; 
+
+		if (($? != 0))
+			then 
+				echo $?
+				date >> $log_err; echo "failed to connnect to $i tcp 80" >> $log_err
+				exit
+			else
+
+			echo $i 
+			date >> $log; echo "connected to $i tcp 80"  >> $log
+	
+		fi
+
+
+		let "j += 1"
+	done
+
+done

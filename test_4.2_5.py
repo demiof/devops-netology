392f31471105b7e3093778b4a55f0bb023ae10fe
Denis <demi@corp.linkintel.ru>
2022-03-29 08:40:26 +0300

    autocommit

--- test_4.2_5.py
+++ test_4.2_5.py
@@ -135,7 +135,7 @@ print(output)
 #\"base\": \""+master_branch_name+"\"}'"]
 
 r=requests.get(github_base_url, auth=(login_name,PAT_name))
-print(r.text)
+#print(r.text)
 r1=requests.post(github_base_url+"/repos/"+login_name+"/"+repo_name+"/pulls", data={"\"head\"":"\""+new_branch_name+"\"","\"base\"":"\""+master_branch_name+"\""})
 print(r1.text)
 
